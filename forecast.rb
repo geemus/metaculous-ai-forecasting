f4158e98e78546b3c91a6596544ba34c3be8dcd2
geemus <geemus@gmail.com>
2025-09-11 11:54:39 -0500

    refactor(anthropic): extract Anthropic API interaction to dedicated class

--- forecast.rb
+++ forecast.rb
@@ -10,79 +10,11 @@ require 'excon'
 require 'formatador'
 require 'json'
 
+require './lib/anthropic'
 require './lib/utility'
 
 Thread.current[:formatador] = Formatador.new
 Thread.current[:formatador].instance_variable_set(:@indent, 0)
-
-# https://docs.anthropic.com/en/api/messages
-def anthropic_completion(*messages)
-  start_time = Time.now
-  response = Excon.post(
-    'https://api.anthropic.com/v1/messages',
-    # expects: 200,
-    headers: {
-      'accept': 'application/json',
-      'anthropic-version': '2023-06-01',
-      'content-type': 'application/json',
-      'x-api-key': ENV['ANTHROPIC_API_KEY']
-    },
-    body: {
-      # model: 'claude-opus-4-1-20250805',
-      model: 'claude-sonnet-4-20250514',
-      max_tokens: 2048,
-      messages: messages,
-      system: <<~SYSTEM,
-        You are an experienced superforecaster.
-
-        - Break down complex questions into smaller, measurable parts, evaluate each separately, then synthesize.
-        - Begin forecasts from relevant base rates (outside view) before adjusting to specifics (inside view).
-        - When evaluating complex uncertainties, consider what is known for certain, what can be estimated, and what remains unknown or uncertain.
-        - Embrace uncertainty by recognizing limits of knowledge and avoid false precision.
-        - Assign precise numerical likelihoods, like 42%, avoiding vague categories or over-precise decimals.
-        - Actively seek out dissenting perspectives and play devil’s advocate to challenge your own views.
-        - Explicitly identify key assumptions, rigorously test their validity, and consider how changing them would affect your forecast.
-        - Use incremental Bayesian updating to continuously revise your probabilities as new evidence becomes available.
-        - Use probabilistic language such as 'there is a 42% chance', 'it is plausible', or 'roughly 42% confidence', and avoid absolute statements to reflect uncertainty.
-        - Balance confidence—be decisive but calibrated, avoiding both overconfidence and excessive hedging.
-        - Maintain awareness of cognitive biases and actively correct for them.
-        - Before your response, show step-by-step reasoning in clear, logical order starting with <reasoning> on the line before and ending with </reasoning> on the line after.
-      SYSTEM
-      temperature: 0.1
-    }.to_json,
-    read_timeout: 360
-  )
-  duration = Time.now - start_time
-  json = JSON.parse(response.body)
-  display_anthropic_meta(json, duration)
-  json
-rescue Excon::Error => e
-  puts e
-  exit
-end
-
-# NOTE: Anthropic API doesn't appear to return cost data
-def display_anthropic_meta(json, duration)
-  Formatador.display_line(
-    format(
-      '[light_green](%<input_tokens>d -> %<output_tokens>d tokens in %<minutes>dm %<seconds>ds)[/]',
-      {
-        input_tokens:
-          json.dig('usage', 'input_tokens') +
-          json.dig('usage', 'cache_creation_input_tokens') +
-          json.dig('usage', 'cache_read_input_tokens'),
-        output_tokens: json.dig('usage', 'output_tokens'),
-        minutes: duration / 60,
-        seconds: duration % 60
-      }
-    )
-  )
-end
-
-def extract_anthropic_content(json)
-  text_array = json['content'].select { |content| content['type'] == 'text' }
-  text_array.map { |content| content['text'] }.join("\n")
-end
 
 # https://docs.perplexity.ai/api-reference/chat-completions-post
 def perplexity_completion(*messages)
@@ -243,16 +175,15 @@ puts research_feedback_prompt
 
 puts
 Formatador.display '[bold][green]## Superforecaster: Reviewing Research…[/] '
-research_feedback_json = anthropic_completion({ 'role': 'user', 'content': research_feedback_prompt })
-research_feedback_content = extract_anthropic_content(research_feedback_json)
-puts extract_xml('feedback', research_feedback_content)
+research_feedback = Anthropic.eval({ 'role': 'user', 'content': research_feedback_prompt })
+puts research_feedback.content
 
 puts
 Formatador.display '[bold][green]# Researcher: Revising Research…[/] '
 revision_json = perplexity_completion(
   { 'role': 'user', 'content': research_prompt },
   { 'role': 'assistant', 'content': strip_xml('reasoning', research_content) },
-  { 'role': 'user', 'content': extract_xml('feedback', research_feedback_content) }
+  { 'role': 'user', 'content': research_feedback.extracted_content('feedback') }
 )
 revision_output = format_research(revision_json)
 puts revision_output
@@ -278,10 +209,9 @@ puts forecast_prompt
 
 puts
 Formatador.display '[bold][green]## Superforecaster: Forecasting…[/] '
-forecast_json = anthropic_completion({ 'role': 'user', 'content': forecast_prompt })
-forecast_content = extract_anthropic_content(forecast_json)
-puts extract_xml('forecast', forecast_content)
+forecast = Anthropic.eval({ 'role': 'user', 'content': forecast_prompt })
+puts forecast.extracted_content('forecast')
 
 puts
 Formatador.display_line '[bold][green]## Forecast[/]'
-puts "#{question['title']} #{extract_xml('probability', forecast_content)}"
+puts "#{question['title']} #{forecast.extracted_content('probability')}"
---
+++ lib/anthropic.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+class Anthropic
+  SYSTEM_PROMPT = <<~SYSTEM_PROMPT
+    You are an experienced superforecaster.
+
+    - Break down complex questions into smaller, measurable parts, evaluate each separately, then synthesize.
+    - Begin forecasts from relevant base rates (outside view) before adjusting to specifics (inside view).
+    - When evaluating complex uncertainties, consider what is known for certain, what can be estimated, and what remains unknown or uncertain.
+    - Embrace uncertainty by recognizing limits of knowledge and avoid false precision.
+    - Assign precise numerical likelihoods, like 42%, avoiding vague categories or over-precise decimals.
+    - Actively seek out dissenting perspectives and play devil’s advocate to challenge your own views.
+    - Explicitly identify key assumptions, rigorously test their validity, and consider how changing them would affect your forecast.
+    - Use incremental Bayesian updating to continuously revise your probabilities as new evidence becomes available.
+    - Use probabilistic language such as 'there is a 42% chance', 'it is plausible', or 'roughly 42% confidence', and avoid absolute statements to reflect uncertainty.
+    - Balance confidence—be decisive but calibrated, avoiding both overconfidence and excessive hedging.
+    - Maintain awareness of cognitive biases and actively correct for them.
+    - Before your response, show step-by-step reasoning in clear, logical order starting with <reasoning> on the line before and ending with </reasoning> on the line after.
+  SYSTEM_PROMPT
+
+  def self.eval(*messages)
+    new.eval(*messages)
+  end
+
+  # https://docs.anthropic.com/en/api/messages
+  def eval(*messages)
+    start_time = Time.now
+    response = connection.post(
+      body: {
+        # model: 'claude-opus-4-1-20250805',
+        model: 'claude-sonnet-4-20250514',
+        max_tokens: 2048,
+        messages: messages,
+        system: SYSTEM_PROMPT,
+        temperature: 0.1
+      }.to_json
+    )
+    result = Response.new(
+      duration: Time.now - start_time,
+      json: JSON.parse(response.body)
+    )
+    result.display_meta
+    result
+  rescue Excon::Error => e
+    puts e
+    exit
+  end
+
+  private
+
+  def connection
+    Excon.new(
+      'https://api.anthropic.com/v1/messages',
+      expects: 200,
+      headers: {
+        'accept': 'application/json',
+        'anthropic-version': '2023-06-01',
+        'content-type': 'application/json',
+        'x-api-key': ENV['ANTHROPIC_API_KEY']
+      },
+      read_timeout: 360
+    )
+  end
+
+  class Response
+    attr_accessor :duration, :json
+
+    def initialize(duration:, json:)
+      @duration = duration
+      @json = json
+    end
+
+    # NOTE: Anthropic API doesn't appear to return cost data
+    def display_meta
+      Formatador.display_line(
+        format(
+          '[light_green](%<input_tokens>d -> %<output_tokens>d tokens in %<minutes>dm %<seconds>ds)[/]',
+          {
+            input_tokens: input_tokens,
+            output_tokens: output_tokens,
+            minutes: duration / 60, seconds: duration % 60
+          }
+        )
+      )
+    end
+
+    def content
+      @content ||= begin
+        text_array = @json['content'].select { |content| content['type'] == 'text' }
+        text_array.map { |content| content['text'] }.join("\n")
+      end
+    end
+
+    def extracted_content(tag)
+      extract_xml(tag, content)
+    end
+
+    def input_tokens
+      @input_tokens ||= @json['usage'].fetch_values('input_tokens', 'cache_creation_input_tokens', 'cache_read_input_tokens').sum
+    end
+
+    def output_tokens
+      @output_tokens ||= @json.dig('usage', 'output_tokens')
+    end
+
+    def stripped_content(tag)
+      strip_xml(tag, content)
+    end
+  end
+end
