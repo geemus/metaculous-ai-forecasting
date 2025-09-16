ea844440c5d86879c75db71f774ac9fafe04149f
geemus <geemus@gmail.com>
2025-09-16 16:42:50 -0500

    feat: add script to open Metaculus questions by ID

---
+++ open.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+# metaculus test questions: (binary: 578, numeric: 14333, multiple-choice: 22427, discrete: 38880)
+post_id = ARGV[0] || raise('post id argument is required')
+
+`open https://metaculus.com/questions/#{post_id}`
