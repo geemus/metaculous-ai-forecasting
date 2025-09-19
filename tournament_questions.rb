2ebb6c00c290daa16736615b8c2e8e9ef2328a97
geemus <geemus@gmail.com>
2025-09-19 15:54:54 -0500

    fix(ci): change workflow to handle single post_id instead of multiple

--- tournament_questions.rb
+++ tournament_questions.rb
@@ -6,4 +6,4 @@ require './lib/metaculus'
 tournament_id = ARGV[0] || raise('tournament id argument is required')
 
 questions = Metaculus.list_tournament_questions(tournament_id)
-puts questions.map(&:post_id).join(' ')
+puts questions.first.post_id
