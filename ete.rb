2385acdcffb4cbb26fef0d97741cf1900aa0ec98
geemus <geemus@gmail.com>
2025-10-16 16:54:58 -0500

    feat: dynamically use forecasters from provider module
    - Replace hardcoded forecaster indices with dynamic count from Provider
    - Add require statement for provider library
    - Remove unnecessary commented metadata
    - Simplify forecasting and revision loops to use dynamic forecaster count

--- ete.rb
+++ ete.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# metaculus test questions: (binary: 578, numeric: 14333, multiple-choice: 22427, discrete: 38880)
-# metaculus tournaments: minibench, fall-aib-2025
+require_relative('lib/provider')
+
 raise('tournament id or post id argument is required') unless ARGV[0]
 
 id = ARGV[0] || raise('post id argument is required')
@@ -21,12 +21,12 @@ end
 system "./research.rb #{post_id}"
 system "echo $(cat tmp/#{post_id}/post.json | jq -r '.title')"
 system "echo $(cat tmp/#{post_id}/post.json | jq -r '.question.description')"
-0.upto(2).each do |forecaster_index|
+Provider::FORECASTERS.count.times do |forecaster_index|
   fork { system("./forecast.rb #{post_id} #{forecaster_index}") }
 end
 Process.waitall
 system "./forecast_stats.rb #{post_id} forecast"
-0.upto(2).each do |forecaster_index|
+Provider::FORECASTERS.count.times do |forecaster_index|
   fork { system("./revise_forecast.rb #{post_id} #{forecaster_index}") }
 end
 Process.waitall
